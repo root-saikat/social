6b6e6264129a1b62658c5dada4e3a6e09fc24521
root-saikat <44300226+root-saikat@users.noreply.github.com>
2022-05-04 14:20:51 +0600

    messenger window

--- jscript.js
+++ jscript.js
@@ -9,3 +9,12 @@ drkbtn.onclick = function(){
     drkbtn.classList.toggle("drkbtn-on");
     document.body.classList.toggle("drk-theme");
 }
+
+
+
+// messenger
+var messengerui = document.querySelector(".messenger-ui");
+
+function messengeruitoggle(){
+    messengerui.classList.toggle("messenger-ui-height"); 
+}
